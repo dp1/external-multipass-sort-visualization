24d3a673756e753b24b338dca3894c32d2f97602
Kanton99 <avolkov99@gmail.com>
2022-08-22 16:25:15 +0200

    working on image generation

--- UI/UI.py
+++ UI/UI.py
@@ -1,3 +1,6 @@
+from numbers import Integral
+import numbers
+from subprocess import CREATE_NEW_CONSOLE
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
@@ -23,12 +26,32 @@ class UI:
         Button(self.root,text="sort").grid(column=1,row=1)
 
         #canvas
-        canvas = Canvas(self.root,bg="white")
-        canvas.grid(column=0,row=2,columnspan=20,rowspan=10,sticky=tk.N+tk.E+tk.S+tk.W)
+        self.canvas = Canvas(self.root,bg="white")
+        self.canvas.grid(column=0,row=2,columnspan=20,rowspan=10,sticky=tk.N+tk.E+tk.S+tk.W)
 
-        canvas.create_rectangle(1,1,20,20,fill="red")
+        #canvas.create_rectangle(1,1,20,20,fill="red")
 
+        #frame = Frame(2,2,self.canvas)
+        self.frames(11)
         self.root.mainloop()
+
+    def frames(self, n: numbers):
+        self.canvas.update()
+        width = self.canvas.winfo_width()
+        height = self.canvas.winfo_height()
+        print(width)
+        print(height)
+        posx=int(width/2)
+        posy=int(height/2)
+
+        posx = int((n*22)/2)
+        frames=[]
+        for i in range(n):
+            frame = myFrame(posx,posy,self.canvas)
+            posx+=22
+            frames.append(frame)
+
+        pass
 
     def validate(self, P):
         try:
@@ -36,4 +59,15 @@ class UI:
             return True
         except ValueError:
             return False
+
+class myFrame:
+    def __init__(self,posx,posy, canvas: Canvas) -> None:
+        #position of upper left corner of frame
+        self.x = posx
+        self.y = posy
+
+        self.color = "red"
+
+        canvas.create_rectangle(self.x,self.y,self.x+20,self.y+20,fill=self.color)
+        pass
 ui = UI()
