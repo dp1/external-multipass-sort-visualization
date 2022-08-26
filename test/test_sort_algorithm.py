6b0239fa41df2751486252ae0b95bb0fa9990a3d
Dario Petrillo <dario.pk1@gmail.com>
2022-08-26 11:09:26 +0200

    Update unit tests

--- test/test_sort_algorithm.py
+++ test/test_sort_algorithm.py
@@ -1,4 +1,5 @@
 import unittest
+import copy
 from visualizer.sort_algorithm import Sort
 
 class TestSort(unittest.TestCase):
@@ -7,7 +8,9 @@ class TestSort(unittest.TestCase):
             for F in [2, 5, 10, 100]:
 
                 sort = Sort(B, F)
-                original_data = sort.relation.copy()
+                original_data = copy.deepcopy(sort.relation)
+                original_tuples = sum([x.data for x in original_data], [])
+                # print(original_tuples)
 
                 print(f'Running sort for B = {B}, F = {F}')
                 print(f'Data: {original_data}')
@@ -15,10 +18,18 @@ class TestSort(unittest.TestCase):
                 sort.sort()
 
                 # The buffer should contain F items
-                assert(len(sort.buffer) == F)
+                self.assertEqual(len(sort.buffer), F)
+
                 # The relation should contain B items
-                assert(len(sort.relation) == B)
+                self.assertEqual(len(sort.relation), B)
+
                 # No data should be left in memory
-                assert(all([item.empty for item in sort.buffer]))
+                for frame in sort.buffer:
+                    self.assertTrue(all([item.empty for item in frame.data]))
+
                 # The relation should be sorted
-                assert(sort.relation == sorted(original_data))
+                result_tuples = sum([x.data for x in sort.relation], [])
+                self.assertEqual(result_tuples, sorted(original_tuples))
+
+if __name__ == '__main__':
+    unittest.main()
